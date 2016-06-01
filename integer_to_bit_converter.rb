d4edb4e0932c1f5717549cccd9dd31402227c9ba
Ty Kelly <kellytyquan@gmail.com>
2016-06-01 09:12:20 -0400

    added test file

--- integer_to_bit_converter.rb
+++ integer_to_bit_converter.rb
@@ -36,20 +36,3 @@ def to_binary_digit(n)
 
  arr.map(&:to_s).join 
 end
-
-puts 'to_binary_digit(0) == 0: ' + (to_binary_digit(0) == "0").to_s
-puts 'to_binary_digit(1) == 1: ' + (to_binary_digit(1) == "1").to_s
-puts 'to_binary_digit(2) == 10: ' + (to_binary_digit(2) == "10").to_s
-puts 'to_binary_digit(3) == 11: ' + (to_binary_digit(3) == "11").to_s
-puts 'to_binary_digit(4) == 100: ' + (to_binary_digit(4) == "100").to_s
-puts 'to_binary_digit(5) == 101: ' + (to_binary_digit(5) == "101").to_s
-puts 'to_binary_digit(6) == 110: ' + (to_binary_digit(6) == "110").to_s
-puts 'to_binary_digit(7) == 111: ' + (to_binary_digit(7) == "111").to_s
-puts 'to_binary_digit(8) == 1000: ' + (to_binary_digit(8) == "1000").to_s
-puts 'to_binary_digit(9) == 1001: ' + (to_binary_digit(9) == "1001").to_s
-puts 'to_binary_digit(22) == 10110: ' + (to_binary_digit(22) == "10110").to_s
-puts 'to_binary_digit(86) == 1010110: ' + (to_binary_digit(86) == "1010110").to_s
-puts 'to_binary_digit(92) == 1011100: ' + (to_binary_digit(92) == "1011100").to_s
-puts 'to_binary_digit(30) == 11110: ' + (to_binary_digit(30) == "11110").to_s
-puts 'to_binary_digit(50) == 110010: ' + (to_binary_digit(50) == "110010").to_s
-puts 'to_binary_digit(100) == 1100100: ' + (to_binary_digit(100) == "1100100").to_s
